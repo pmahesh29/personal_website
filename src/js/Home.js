a4d3711ffb185fddf0d5c65f0f8157c618a3106d
pmahesh29 <pmahesh@uark.edu>
2025-11-10 01:27:00 -0600

    Update About section with new subtitle and enhanced description

--- src/js/Home.js
+++ src/js/Home.js
@@ -6,11 +6,25 @@ const About = () => (
   <section className="about">
     <div className="container">
       <div className="about-content">
-        <h2> Pranav Mahesh</h2>
+        <h2> Pranav Mahesh
+        <p className="subtitle">Application Support Intern @ Arvest Bank</p>
+        </h2>
         <div className="about-text">
         <img src={myPhoto} alt="Pranav Mahesh" className="about-photo" />
-          <p>Hello, I'm Pranav Mahesh, a fourth-year Computer Science major at the University of Arkansas with a focus on Cybersecurity. <br></br>My experience includes internships in data security and software development, where I've developed practical skills in various programming languages and tools. I'm also actively involved in fostering a community of like-minded individuals as the Vice-President of CyberHogs. </p>
-        </div>
+          <p>
+            Hey, I’m Pranav Mahesh. I am someone who’s always curious about how things work,
+            whether it’s technology, people, or the world around me. I’m passionate about cybersecurity,
+            tech exploration, and building things from the ground up but I’m just as driven by creativity,
+            problem-solving, and personal growth.
+            <br></br> <br></br>
+            I am a computer science graduate with a concentration in Cybersecurity from the University of Arkansas.
+            Experienced in virtual machines, programming, web development, and data security through academic 
+            projects and hands-on internships. I also served as the Vice President of CyberHogs, the university’s 
+            cybersecurity club, where I helped organize technical events and connect students with learning resources 
+            in the field.
+          </p>
+
+          </div>
       </div>
     </div>
   </section>
